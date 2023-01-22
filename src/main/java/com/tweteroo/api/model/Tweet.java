1d6888adf39767c4ba73abfbc2dcb6f6e13c5f78
lucas-guarnieri <lucaslguarnieri@gmail.com>
2023-01-22 15:58:22 -0300

    feat - user and tweet models for database

---
+++ src/main/java/com/tweteroo/api/model/Tweet.java
@@ -0,0 +1,27 @@
+package com.tweteroo.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String avatar;
+
+    @Column(nullable = false, length = 280)
+    private String text;
+}
---
+++ src/main/java/com/tweteroo/api/model/User.java
@@ -0,0 +1,24 @@
+package com.tweteroo.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String avatar;
+}
